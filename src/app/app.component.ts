61ac5de9e3797349cfa820aa37b23ed6e76a129e
Yuri An <yuriandev@gmail.com>
2022-08-15 17:09:38 +0300

    feat: add filtering completed items and adding new items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,15 @@ export class AppComponent {
   }
 
   get items(): readonly TodoItem[] {
-    return this.list.items;
+    return this.list.items.filter(
+      (item) => this.showComplete || !item.complete
+    );
   }
+
+  addItem(newItem: string) {
+    if (newItem != '') {
+      this.list.addItem(newItem);
+    }
+  }
+  showComplete: boolean = false;
 }
