e1e633d20a40ce487c9511ff5f8eaeb70e8fbc91
Yuri An <yuriandev@gmail.com>
2022-08-15 13:42:39 +0300

    feat: create todoList and todoItem components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList('Bob', [
+    new TodoItem('Go for run', true),
+    new TodoItem('Get flowers'),
+    new TodoItem('Collect tickets'),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter((item) => !item.complete).length;
+  }
 }
