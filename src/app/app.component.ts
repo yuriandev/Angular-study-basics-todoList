b00ed45eaacd02fbea58d41e292c3fa1f0488e0a
Yuri An <yuriandev@gmail.com>
2022-08-15 16:30:47 +0300

    feat: add styles for list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,4 +21,8 @@ export class AppComponent {
   get itemCount(): number {
     return this.list.items.filter((item) => !item.complete).length;
   }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items;
+  }
 }
